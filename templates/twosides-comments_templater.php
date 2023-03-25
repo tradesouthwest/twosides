c5db0054695f599fb36c7dfea0d039d83126c738
Larry <tradesouthwest@outlook.com>
2023-03-25 16:56:23 -0700

    sanitize key

--- templates/twosides-comments_templater.php
+++ templates/twosides-comments_templater.php
@@ -13,19 +13,14 @@ add_filter( 'comments_template', 'twosides_comment_templater' );
 function twosides_comment_templater( $comment_template ) {
   global $post;
   
-  if ( !( is_singular() && ( have_comments() || 'open' == 
-  $post->comment_status ) ) ) {
+  if ( !( is_singular() && ( have_comments() || 'open' == $post->comment_status ))){
      return;
   }
   
   $post_type_is = 'post';
-  $options      = get_option('twosides_admin'); 
-  $checkbox     = (empty($options['twosides_checkbox_commlists'] )) 
-                   ? 0 : $options['twosides_checkbox_commlists'];
-  //if( is_single() && $checkbox == 0 ) {
 
-    if( $post->post_type == $post_type_is ){ 
+    if( $post->post_type == sanitize_key($post_type_is) ){ 
       return dirname(__FILE__) . '/comments.php';
     }
-  //}
+
 } 
