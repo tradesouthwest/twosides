0060a6af3e1cb3513250e940af7f8e2fbdb1d8d6
Larry <tradesouthwest@outlook.com>
2023-03-25 17:28:43 -0700

    sanitize strings, removed str defaults

--- includes/twosides-shortcodes.php
+++ includes/twosides-shortcodes.php
@@ -24,16 +24,16 @@ function comments_twosides_get_comment_positive_count() {
     ) );
 
     ob_start(); 
-    echo '<p class="twosides-prohead">'. $num_comments .' <span class="prohead-count" style="width:' . $num_comments . 'em"> </span></p>'; 
+    echo '<p class="twosides-prohead">'. absint($num_comments) .' <span class="prohead-count" style="width:' . esc_attr($num_comments) . 'em"> </span></p>'; 
     $comment_pos = ob_get_clean();
-    return $comment_pos;
+    return wp_kses_post($comment_pos);
 }
 /**
 * @returns comment count
 */
 function comments_twosides_get_comment_negative_count() {
-    global $post, $comments, $wp_query; //, $comments_by_type;
-    //$commtype     = get_comment_meta($comment->comment_ID, 'twosides_commtype', true );
+    global $post, $comments, $wp_query; 
+    //$commtype 
     $num_comments = get_comments (
     array(
         'post_id'    => get_the_ID(),
@@ -42,9 +42,9 @@ function comments_twosides_get_comment_negative_count() {
         'count'      => true  
     ) );
 
-    ob_start(); echo '<p class="twosides-conhead">'. $num_comments .' <span class="conhead-count" style="width:'. $num_comments .'em;"> </span></p>'; 
+    ob_start(); echo '<p class="twosides-conhead">'. absint($num_comments) .' <span class="conhead-count" style="width:'. esc_attr($num_comments) .'em;"> </span></p>'; 
     $comment_con = ob_get_clean();
-    return $comment_con;
+    return wp_kses_post($comment_con);
 }
 /**
  * Shortcode 
@@ -55,44 +55,45 @@ function twosides_header_form_shortcode( $atts ='', $content = null)
 { 
     if( is_single() ) :    
         $options = get_option('twosides_admin'); 
-        $def3 = " + ";
+        
           $twosides_positxt     = $options['twosides_positxt'];
-          if( $twosides_positxt == '' ) $twosides_positxt = $def3;  
-        $def4 = " - ";
+          if( $twosides_positxt == '' ) $twosides_positxt     = esc_html(" + ");  
+        
           $twosides_negatxt     = $options['twosides_negatxt'];
-          if( $twosides_negatxt == '' ) $twosides_negatxt = $def4;  
-        $def5 = "Positive Comment";
+          if( $twosides_negatxt == '' ) $twosides_negatxt     = esc_html(" - ");  
+        
           $twosides_posiheader  = $options['twosides_posiheader'];
-          if( $twosides_posiheader == '' ) $twosides_posiheader = $def5;  
-        $def6 = "Negative Comment";
+          if( $twosides_posiheader == '' ) $twosides_posiheader = esc_html("Positive Comment");  
+        
           $twosides_negaheader  = $options['twosides_negaheader'];
-          if( $twosides_negaheader == '' ) $twosides_negaheader = $def6;  
+          if( $twosides_negaheader == '' ) $twosides_negaheader = esc_html("Negative Comment");
           
         $legendP = comments_twosides_get_comment_positive_count();
         $legendN = comments_twosides_get_comment_negative_count();
       
           // display header above comments list
-          ob_start();
+    ob_start();
     echo '
 <div class="comments-above-list">
     
     <fieldset class="twosides_fieldset"> 
     <div class="twosides-left">
-    <legend>' . $legendP .'</legend>
+    <legend>' . balanceTags($legendP) .'</legend>
         <form name="twoside-comment-positive" method="POST" action="#commentform">
             <h4>'. esc_html__( $twosides_posiheader, 'twosides' ) .'</h4>
-            <input type="submit" name="action_positive" 
-            value="'. $twosides_positxt .'" id="actionPositive">
+            <input type="submit" id="actionPositive" name="action_positive" 
+            value="'. esc_attr($twosides_positxt) .'">
             <input id="twsPositive" type="hidden" name="twosides_positive" 
             value="twosides_positive">
         </form>  
     </div>
 
     <div class="twosides-right">
-        <legend>' . $legendN .'</legend>
+        <legend>' . balanceTags($legendN) .'</legend>
         <form name="twoside-comment-negative" method="POST" action="#commentform">
             <h4>'. esc_html__( $twosides_negaheader, 'twosides' ) .'</h4>
-            <input type="submit" name="action_negative" value="'. $twosides_negatxt .'" id="actionNegative">
+            <input type="submit" id="actionNegative" name="action_negative" 
+                value="'. esc_attr($twosides_negatxt) .'">
             <input id="twsNegative" type="hidden" name="twosides_negative" 
             value="twosides_negative">
         </form>
@@ -101,7 +102,7 @@ function twosides_header_form_shortcode( $atts ='', $content = null)
 </div>';
 
     $contents = ob_get_clean();
-        return $contents; 
+        return stripslashes($contents); 
 endif;
 }
 
