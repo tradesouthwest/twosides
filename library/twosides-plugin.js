b391c8e5e0059b42381aa901a3c0740dcffe9cdb
Larry <tradesouthwest@outlook.com>
2023-03-25 17:06:07 -0700

    removed unused code

--- library/twosides-plugin.js
+++ library/twosides-plugin.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($) { 
     $('div#comments').addClass('twosides');
-   /* $('.comment').addClass('twosides-left'); */
+
 });//end noconflict
 jQuery(document).ready(function($) { 
     $( 'li.twosides_positive:last-child' ).addClass('lastpositive');
