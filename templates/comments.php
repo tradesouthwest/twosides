1ecb6c902661a9c35f1968cdf2249dab695ae5cb
Larry <tradesouthwest@outlook.com>
2023-03-31 13:32:43 -0700

    doc block

--- templates/comments.php
+++ templates/comments.php
@@ -12,15 +12,13 @@
  * @since 1.0.1
  */
 global $post, $comments; $commentn = $commentp = '';
-if ( !comments_open() ) {
-  return;
-}
-  /** If the current post is protected by a password and 
-   * the visitor has not yet entered the password we will 
-   * return early without loading the comments. 
-   * */ 
-if( post_password_required() ) { return; } 
 
+if ( !comments_open() ) { return; }
+/* If the current post is protected by a password and 
+ * the visitor has not yet entered the password we will 
+ * return early without loading the comments. 
+ * */ 
+if( post_password_required() ) { return; } 
 ?> 
   
 <div id="comments" class="comments-area"> 
