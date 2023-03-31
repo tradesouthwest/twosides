5850798e8190271e4b9f4721321c443da83eb4f7
Larry <tradesouthwest@outlook.com>
2023-03-31 13:34:40 -0700

    since

--- templates/twosides-comments_templater.php
+++ templates/twosides-comments_templater.php
@@ -4,7 +4,7 @@
  *
  * This is the templater that gets current comments
  * and the comment form. 
- * 
+ * @since 1.0.0
  * @link https://codex.wordpress.org/Plugin_API/Filter_Reference/comments_template
  * @package TwoSides
  */
